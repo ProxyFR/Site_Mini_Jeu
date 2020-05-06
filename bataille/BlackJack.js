1325a4a9481c1cc69025d301e3cc7f8123451c42
Mehdi <bachamehdi.mb@gmail.com>
2020-05-06 22:11:45 +0200

    add the logique to the game

--- bataille/BlackJack.js
+++ bataille/BlackJack.js
@@ -1,7 +1,13 @@
 let suits = ["H", "D", "C", "S"];
 let values = [2,3,4,5,6,7,8,9,10,"J", "Q", "K", "A"];
 let index = 0;
+let dealerIndex = 0;
 let autoDeal;
+let playerCards = [];
+let playerHandValue = 0;
+let dealerCards = [];
+let dealerHandValue = 0;
+
 // New class to create all cards
 class Card {
 	constructor(value, suit) {
@@ -59,6 +65,25 @@ deck.generate();
 deck.shuffle();
 let dealt = deck.deal();
 
+// function that start the game
+
+function startGame() {
+	clean();
+	deck.generate();
+	deck.shuffle();
+	dealt = deck.deal();
+	playerCards = [];
+	dealerCards = [];
+	document.querySelector(".endgame").style.display = "none";
+	firstDeal();
+	playerCards.push((dealt[1].value), (dealt[3].value));
+	dealerCards.push((dealt[2].value));
+	dealerHandAddValue();
+	playerHandAddValue();
+}
+
+startGame();
+
 // function that auto deal 4 cards at the begining of the game
 
 function firstDeal(){
@@ -68,13 +93,12 @@ function firstDeal(){
 // function that add class to the card to show them on screen
 
 function showCard(){
-	// console.log(index);
 	let cardName = dealt[index].name;
 	let cardID = document.getElementById(cardName);
 	cardID.classList.add("visible" + index);
-	index++;
+	index ++;
 	// Stop the first Deal after 4 cards are dealt
-	if (index > 3) {
+	if (index >= 4) {
 		clearInterval(autoDeal);
 	}
 }
@@ -87,6 +111,26 @@ function reveal(){
 	hiddenCardId.classList.add("reveal");
 }
 
+function displayDealerCards(){
+	test = setInterval(dealerhit, 500);
+}
+
+function dealerhit(){
+	if (dealerHandValue < 17){
+		let cardName = dealt[index].name;
+		let cardID = document.getElementById(cardName);
+		cardID.classList.add("dealer" + dealerIndex);
+		dealerCards.push((dealt[index].value))
+		dealerHandAddValue();
+		index ++;
+		dealerIndex ++;
+	} if (dealerHandValue >= 17) {
+		clearInterval(test);
+		dealerIndex = 0;
+		compareHands();
+	}
+}
+
 // function that reset every class modification on card div
 
 function clean(){
@@ -94,6 +138,13 @@ function clean(){
 	let visible1 = document.getElementsByClassName("visible1");
 	let visible2 = document.getElementsByClassName("visible2");
 	let visible3 = document.getElementsByClassName("visible3");
+	let visible4 = document.getElementsByClassName("visible4");
+	let visible5 = document.getElementsByClassName("visible5");
+	let visible6 = document.getElementsByClassName("visible6");
+	let visible7 = document.getElementsByClassName("visible7");
+	let dealer0 = document.getElementsByClassName("dealer0");
+	let dealer1 = document.getElementsByClassName("dealer1");
+	let dealer2 = document.getElementsByClassName("dealer2");
 	let reveal = document.getElementsByClassName("reveal");
 	if (visible0.length != 0 && visible0){
 		visible0[0].classList.remove("visible0");
@@ -111,15 +162,108 @@ function clean(){
 		visible3[0].classList.remove("visible3");
 		index = 0;
 	}
+	if (visible4.length != 0 && visible4){
+		visible4[0].classList.remove("visible4");
+		index = 0;
+	}
+	if (visible5.length != 0 && visible5){
+		visible5[0].classList.remove("visible5");
+		index = 0;
+	}
+	if (visible6.length != 0 && visible6){
+		visible6[0].classList.remove("visible6");
+		index = 0;
+	}
+	if (visible7.length != 0 && visible7){
+		visible7[0].classList.remove("visible7");
+		index = 0;
+	}
+	if (dealer0.length != 0 && dealer0){
+		dealer0[0].classList.remove("dealer0");
+		index = 0;
+	}
+	if (dealer1.length != 0 && dealer1){
+		dealer1[0].classList.remove("dealer1");
+		index = 0;
+	}
+	if (dealer2.length != 0 && dealer2){
+		dealer2[0].classList.remove("dealer2");
+		index = 0;
+	}
 	if (reveal.length != 0 && reveal){
 		reveal[0].classList.remove("reveal");
 		index = 0;
 	}
 }
-startGame();
 
-// function that start the game
+function hit() {
+	playerCards.push((dealt[index].value))
+	showCard();
+	playerHandAddValue();
+	checkLose();
+}
 
-function startGame() {
-	firstDeal();
+function stand() {
+	reveal();
+	dealerCards.push((dealt[0].value))
+	dealerHandAddValue();
+	displayDealerCards();
+}
+
+function playerHandAddValue() {
+	playerHandValue = 0;
+	for (var i = 0; i < playerCards.length; i++) {
+		if (playerCards[i] == "J" || playerCards[i] == "Q" || playerCards[i] == "K") {
+			playerHandValue += 10;
+		} else if (playerCards[i] == "A" && (playerHandValue + 11) > 21 ) {
+			playerHandValue ++;
+		} else if (playerCards[i] == "A" && (playerHandValue + 11) <= 21 ) {
+			playerHandValue += 11;
+		} else {
+			playerHandValue += playerCards[i];
+		}
+	}
+	document.querySelector(".playerHandValue").innerText = playerHandValue;
+}
+
+function dealerHandAddValue() {
+	dealerHandValue = 0;
+	for (var i = 0; i < dealerCards.length; i++) {
+		if (dealerCards[i] == "J" || dealerCards[i] == "Q" || dealerCards[i] == "K") {
+			dealerHandValue += 10
+		}else if (dealerCards[i] == "A" && (dealerHandValue + 11) > 21 ) {
+			dealerHandValue ++;
+		} else if (dealerCards[i] == "A" && (dealerHandValue + 11) <= 21 ) {
+			dealerHandValue += 11;
+		} else {
+			dealerHandValue += dealerCards[i];
+		}
+	}
+	document.querySelector(".dealerHandValue").innerText = dealerHandValue;
+}
+
+function compareHands() {
+	if (dealerHandValue <= 21 && dealerHandValue > playerHandValue) {
+		document.querySelector(".text").innerText = "You lose !";
+		document.querySelector(".endgame").style.display = "block";
+	} else if (dealerHandValue <= 21 && dealerHandValue < playerHandValue) {
+		document.querySelector(".text").innerText = "You Win !";
+		document.querySelector(".endgame").style.display = "block";
+	} else if (dealerHandValue == playerHandValue) {
+		document.querySelector(".text").innerText = "It's a tie";
+		document.querySelector(".endgame").style.display = "block";
+	}else if (dealerHandValue > 21) {
+		document.querySelector(".text").innerText = "You Win !";
+		document.querySelector(".endgame").style.display = "block";
+	}
+}
+
+
+function checkLose() {
+	if (playerHandValue > 21) {
+		document.querySelector(".action").innerText = "You lose !";
+		// Display the "Eng Game" Screen
+		document.querySelector(".endgame").style.display = "block";
+		stand();
+	}
 }
