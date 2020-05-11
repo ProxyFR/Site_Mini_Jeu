b024cfb9325f062c528b503b1c6ca508fb3eac8d
Mehdi <bachamehdi.mb@gmail.com>
2020-05-11 18:37:41 +0200

    creating snake branch and files

---
+++ snake/snake.js
@@ -0,0 +1,7 @@
+
+console.log("hello");
+// const canvas = document.getElementById("snake");
+// const ctx = canvas.getContext('2d');
+
+// ctx.fillStyle = 'green';
+// ctx.fillRect(10, 10, 100, 100);
